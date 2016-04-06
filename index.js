def7b154f1082f1770b925197af05c6199e54763
Praful Rana <prafulrana@gmail.com>
2016-04-06 16:14:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ var bot = new Bot(settings);
 
 bot.on('start', function() {
     bot.postMessageToChannel('some-channel-name', 'Hello channel!');
-    bot.postMessageToUser('some-username', 'hello bro!');
+    bot.postMessageToUser('praful', 'hello bro!');
     bot.postMessageToGroup('some-private-group', 'hello group chat!');
 });
