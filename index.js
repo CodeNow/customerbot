1c86a77d081386d5a29b0ba97e7627df092ea8ca
Praful Rana <prafulrana@gmail.com>
2016-05-16 16:24:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class MyCmd extends Command {
 
     // Override the default handler (!)
     handler(data, ctx, slack, callback) {
-        console.log(data);
+        console.log("oh munna" + data);
         // data = incoming message data. I recommend console.logging it
         // ctx = commandtext. Array of words in message. Log it.
         // slack = SlackBot instance. Use it to do slack.getUser(data.user).name
