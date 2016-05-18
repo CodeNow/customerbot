fa8c38e299a71fde95363b8ef4576331dcdaf712
Praful Rana <prafulrana@gmail.com>
2016-05-18 15:35:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,3 @@
-// var Bot = require('slackbots');
-
-// // create a bot
-// var settings = {
-//     token: process.env.BOT_API_KEY,
-//     name: 'My Bot'
-// };
-// var bot = new Bot(settings);
-
-// bot.on('start', function() {
-//     bot.postMessageToUser('praful', 'hello bro!');
-// });
-
-// bot.on('message', function(data) {
-//     // all ingoing events https://api.slack.com/rtm 
-//     bot.postMessageToUser(data.user, 'hi', function(data) {/* ... */});
-//     console.log(data);
-// });
-
-// 'use strict'; // Important! This uses many ES6 features.
-
-// var slack = require('easy-slackbot'); // or './index' if running locally
-// var SlackBot = slack.Bot;
-// var Command = slack.Command;
-
-// // Create your custom command
-// class MyCmd extends Command {
-//     constructor(commands) {
-//         // Commands are the commands that will trigger your handler
-//         // can be 'string' or ['string', 'array']
-//         super(commands);
-//     }
-
-//     // Override the default handler (!)
-//     handler(data, ctx, slack, callback) {
-//         console.log("oh munna" + data);
-//         // data = incoming message data. I recommend console.logging it
-//         // ctx = commandtext. Array of words in message. Log it.
-//         // slack = SlackBot instance. Use it to do slack.getUser(data.user).name
-//         // callback = call this function with your response message.
-
-//         // This scope is NOT MyCmd. It is the SlackBot scope.
-//         // To access private methods, I recommend making them
-//         // static and accessing them through `MyCmd.Method()`
-
-//         var response = 'Hello there!';
-//         callback(response);
-//     }
-// }
-
-// // Set up the bot
-// var bot = new SlackBot({
-//     // API Auth token
-//     token: process.env.BOT_API_KEY,
-//     // Botname from @botmention
-//     name: 'My Bot',
-//     // Provide a onTeamJoin handler
-//     welcome: function(data) {
-//         callback('Welcome!');
-//     },
-//     // Command prefix
-//     prefix: '!',
-//     // Pass any custom commands
-//     commands: [
-//         new MyCmd(['hi', 'hello', 'hey']),
-//     ]
-// });
-
-// // Add an anonymous function if you can't be bothered to
-// // set up a whole extends Command class.
-// bot.addCommandDirectly('help', (data, ctx, slack, callback) => {
-//     console.log("hello");
-//     // callback('Read the docs, *fool*.');
-// });
-
-// // Connect async
-// bot.connect();
-
 var Slack = require('@slack/client');  
 var RtmClient = Slack.RtmClient;  
 var RTM_EVENTS = Slack.RTM_EVENTS;
@@ -88,5 +10,13 @@ rtm.start();
 rtm.on(RTM_EVENTS.MESSAGE, function(message) {  
   var channel = message.channel;
   var text = message.text;
-  rtm.sendMessage(text, channel);
+
+  if (text == "tags") {
+    setTimeout(1000, function (){
+      rtm.sendMessage("these are all the tags from intercom", channel);
+    });
+  } else  {
+    rtm.sendMessage("I do not understand this command", channel);
+  }
+
 });
