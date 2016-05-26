595187f9eb36ecb75f2a646127683667d5b4632d
Praful Rana <prafulrana@gmail.com>
2016-05-26 11:27:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -140,8 +140,8 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 	    	// send error message
 	    } else {
 	    	getIssueTable(issue, function (err, results){
-	    		console.log(results);
-	    		rtm.sendMessage("here they are" + results, channel);
+	    		// console.log(results);
+	    		rtm.sendMessage("here they are" + results.toString(), channel);
 	    	});
 	    }
 	});
