f390fb026df3f8882821908c088cecaa8d7de410
Praful Rana <prafulrana@gmail.com>
2016-05-27 15:21:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,15 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 	});
 	
   } else  {
-    rtm.sendMessage("I do not understand this command", channel);
+  	var Table = require('cli-table');
+	var table = new Table({ head: ["", "Top Header 1", "Top Header 2"] });
+	 
+	table.push(
+	    { 'Left Header 1': ['Value Row 1 Col 1', 'Value Row 1 Col 2'] }
+	  , { 'Left Header 2': ['Value Row 2 Col 1', 'Value Row 2 Col 2'] }
+	);
+	 
+    rtm.sendMessage("table.toString(), channel);
   }
 
 });
