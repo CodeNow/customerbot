185a01cdcbb880cf2dcdd35af35ca17828b709d8
Praful Rana <prafulrana@gmail.com>
2016-06-03 11:22:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,14 @@ bot.on('message', function(data) {
 						if (!company) {
 							bot.postMessage(data.channel, 'Company does not exist in Intercom; format for tagging = tag <company name> <SAN number> <notes>').fail(function (errr) {console.log(errr.toString);});
 						} else {
-							bot.postMessage(data.channel, 'Going to tag company').fail(function (errr) {console.log(errr.toString);});
+							client.tags.tag({ name: issue, companies: [{ id: company.id }] }, function (err, res) {
+								
+								
+								if (err)
+									bot.postMessage(data.channel, 'there was an error tagging this company').fail(function (errr) {console.log(errr.toString);});
+								else 
+									bot.postMessage(data.channel, 'Company tagged').fail(function (errr) {console.log(errr.toString);});
+							});
 						}
 					})
 				}
